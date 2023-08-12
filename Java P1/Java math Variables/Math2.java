b84ddab6db6293e1df5a5082024e0254c93e703b
Abrar Zahin <132545733+stormshadow-07@users.noreply.github.com>
2023-08-12 20:31:09 +0600

    Add files via upload

---
+++ Java P1/Java math Variables/Math2.java
@@ -0,0 +1,12 @@
+public class Math2 {
+  public static void main(String[] args) {
+    int a = 5;
+    int b = 6;
+    int c = 8;
+    int d = 3;
+    System.out.println(b + d);
+    System.out.println(a - b);
+    System.out.println(c * b);
+    System.out.println(d / b);
+  }
+}
