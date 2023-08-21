c9a169a26f714047d4d25d2c48e955f4dc6ee2e0
Abrar Zahin <132545733+stormshadow-07@users.noreply.github.com>
2023-08-21 15:01:09 +0600

    Add files via upload

---
+++ Even Odd Number/EvenOdd.java
@@ -0,0 +1,17 @@
+public class EvenOdd {
+
+    public static void main(String[] args) {
+        int n = 10;
+        if (n % 2 == 0) {
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+    }
+
+
+
+
+
+}
