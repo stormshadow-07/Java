440095e3edd9adbbca498cf286877e39cba7e321
Abrar Zahin <132545733+stormshadow-07@users.noreply.github.com>
2023-09-01 22:45:59 +0600

    Add files via upload

---
+++ Byte & UserInput/ByteExample.java
@@ -0,0 +1,22 @@
+public class ByteExample {
+
+    public static void main(String[] args) {
+        byte value = 20;
+        System.out.println("value: " + value);
+
+        short s  =  -32767;
+        System.out.println("s: " + s);
+
+        int a = 172727872;
+        System.out.println("a: " + a);
+
+        long l = 8929922992929229902f;
+        System.out.println("l: " + l);
+
+        float f  = 12.0f;
+        System.out.println("f: " + f);
+
+        double d  = 13.89898989898;
+        System.out.println("d: " + d);
+    }
+}
---
+++ Byte & UserInput/UserInputExample.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public  class UserInputExample {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(system.in);
+
+        int a = sc.nextInt();
+        System.out.println("value of a: " + a);
+
+        double d = sc.nextDouble();
+        System.out.println("value of d: " + d);
+
+        char ch = sc.next().charAt(0);
+    }
+}
+    
